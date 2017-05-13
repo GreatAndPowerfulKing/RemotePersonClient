a3c1cd05587bea089be62a009db018babe3cacb6
GreatAndPowerfulKing <pedos.vm@icloud.com>
2017-05-13 11:03:24 +0300

    Initial Commit

---
+++ Parser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parser
+//
+//  Created by iKing on 13.05.17.
+//  Copyright © 2017 iKing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
